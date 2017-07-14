d4f012121bc7fc98e593c73412c123fc2392a42b
Andrew Denty <andrewdenty@gmail.com>
2017-07-14 16:57:26 +0100

    David's masterclass on React

--- shared.js
+++ shared.js
@@ -1,14 +1,91 @@
-var AndrewsComponent = React.createClass ({
-                    doSomething: function() {
-                        alert("The text displaying is:" + this.props.children);
-                    },
-                    render: function() {
-                        return(
-                            <div>
-                <h3>{this.props.user}</h3>
-                <a onClick={this.doSomething} href="#">Click me</a>
+/*
+  ______
+< footer >
+  ------
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/\
+                 ||----w |
+                 ||     ||
+    
+*/
+
+var links = {
+  Cake: {
+    href: '/cake.html',
+    imgsrc: 'https://unsplash.it/300/200/?random',
+    description: 'This is a cake'
+  },
+  Brownies: {
+    href: '/brownies.html',
+    imgsrc: 'https://unsplash.it/300/200/?random',
+    description: 'These are brownies'
+  },
+  Cookies: {
+    href: '/cookies.html',
+    imgsrc: 'https://unsplash.it/300/200/?random',
+    description: 'Here are some tasty cookies'
+  },
+  Flapjack: {
+    href: '/flapjack.html',
+    imgsrc: 'https://unsplash.it/300/200/?random',
+    description: 'Here is some stale flapjack'
+  }
+};
+
+var Footer = React.createClass({
+    render: function () {
+        var page = this.props.page;
+        return (
+            <div>
+            <h3>Cake Menu-</h3>
+                <div className="row">
+                    {Object.keys(links).map(function(name) {
+                        if (page === name) {
+                            return null;
+                        }
+            
+                        return <div key={name} className="col-xs-4">
+                                <a href={links[name].href}>
+                                    <img className="img-responsive" src={links[name].imgsrc} alt={links[name].description} />
+                                    <p>{links[name].description}</p>
+                                </a>
+                            </div>;
+                    })}
                 </div>
-                        );
+            </div>
+        );
+    }
+});
+
+
+
+/*
+var Footer = React.createClass({
+    render: function () {
+        var page = this.props.page;
+        return (
+            <div>
+                 <div className="row">
+                    {page != 'Cake' &&
+                        <div className="col-xs-6 ">
+                        <h3>Cake</h3>
+                        </div>
                     }
-                    
-                });
+                    {page != 'Brownies' &&
+            <div className="col-xs-6 ">
+                        <h3>Brownies</h3>
+            </div>
+                    }
+                    {page != 'Cookies' &&
+            <div className="col-xs-6 ">
+                        <h3>Cookies</h3>
+            
+            </div>
+                    }
+                </div>
+            </div>
+        );
+    }
+});
+*/
