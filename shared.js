56fe67be98b86452000b66bf26ecee0d90bb2f23
Andrew Denty <andrewdenty@users.noreply.github.com>
2017-07-15 17:13:55 +0100

    Added footer

--- shared.js
+++ shared.js
@@ -1,6 +1,6 @@
 /*
   ______
-< footer >
+< more work >
   ------
          \   ^__^ 
           \  (oo)\_______
@@ -33,7 +33,7 @@ var links = {
   }
 };
 
-var Footer = React.createClass({
+var MoreWork = React.createClass({
     render: function () {
         var page = this.props.page;
         return (
@@ -47,7 +47,7 @@ var Footer = React.createClass({
             
                         return <div key={name} className="col-xs-4">
                                 <a href={links[name].href}>
-                                    <img className="img-responsive" src={links[name].imgsrc} alt={links[name].description} />
+                                    <img className="img-responsive light-grey" src={links[name].imgsrc} alt={links[name].description} />
                                     <p className="small">{links[name].description}</p>
                                 </a>
                             </div>;
@@ -59,33 +59,35 @@ var Footer = React.createClass({
 });
 
 
+/* Page footer */
 
-/*
 var Footer = React.createClass({
     render: function () {
-        var page = this.props.page;
         return (
             <div>
-                 <div className="row">
-                    {page != 'Cake' &&
-                        <div className="col-xs-6 ">
-                        <h3>Cake</h3>
-                        </div>
-                    }
-                    {page != 'Brownies' &&
-            <div className="col-xs-6 ">
-                        <h3>Brownies</h3>
-            </div>
-                    }
-                    {page != 'Cookies' &&
-            <div className="col-xs-6 ">
-                        <h3>Cookies</h3>
-            
-            </div>
-                    }
+             <div className="footer-line"></div>
+            <div className="row">
+                <div className="col-sm-4">
+                    <h3>Find me here –</h3>
+                    <p><a href="http://andrewdenty.tumblr.com/" target="_blank">Thoughts on UX</a>
+                        <br/>
+                        <a href="http://www.parttimebackpacker.com/" target="_blank">Personal blog</a>
+                        <br/>
+                        <a href="https://twitter.com/andrewdenty" target="_blank">Twitter</a>
+                        <br/>
+                        <a href="https://uk.linkedin.com/in/andrewdenty/" target="_blank">Linkedin</a></p>
                 </div>
+                <div className="col-sm-6">
+                    <h3>Let's work together –</h3>
+                    <p>
+                        <a href="mailto:andrewdenty@gmail.com">andrewdenty@gmail.com</a>
+                    </p>
+                </div>
+            </div>
+            <div className="spacer"></div>
+            <p className="small">Copyright © 2017 Andrew Denty</p>
+            <div className="spacer"></div>
             </div>
         );
     }
 });
-*/
